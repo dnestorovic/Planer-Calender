46616ebc690624bccfeec8bbfb50354cbde69482
dnestorovic <david.nestorovic@gmail.com>
2021-12-15 21:37:17 +0100

    Add banner opening

---
+++ calender/src/Apps/Calender/components/Banner/Banner.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <div className="ln-banner">
+            <div className='lnm-image-shadow'>
+                <h1>Mind your own business!</h1>
+                <p id='lnp-banner-paragraph-1'>Plan your own time carefully...</p>
+                <p id='lnp-banner-paragraph-2'>...because the time is money!</p>
+                <p id='lnp-banner-paragraph-3'>So don't waste your time, plan it with our app!</p>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
