eac9e9f8bf4e8845f629a3d2ff18e4959e37c2a8
dnestorovic <david.nestorovic@gmail.com>
2021-12-19 20:40:30 +0100

    Add Task services methods for get, update and delete

--- calender/src/services/TasksService.ts
+++ calender/src/services/TasksService.ts
@@ -1,18 +1,43 @@
-import BaseService from "./BaseService";
-import {MonthDataModel} from "../models/MonthDataModel";
+import BaseService from './BaseService';
+import { MonthDataModel } from '../models/MonthDataModel';
 
 interface ITasks {
-    getTasksFormMonth: (month: number) => Promise<MonthDataModel[]>;
+    getById: (id: string) => Promise<MonthDataModel>;
+    getTasksForMonth: (month: number) => Promise<MonthDataModel[]>;
+    getTaskForDay: (day: number, month: number, year: number) => Promise<MonthDataModel[]>;
+    deleteTask: (id: string) => Promise<any>;
+    addTask: (data: MonthDataModel) => Promise<any>;
+    updateTask: (taskData: MonthDataModel, id: string) => Promise<any>;
 }
 
 const TasksService: () => ITasks = () => {
     const baseUrl = 'http://localhost:8001/months/';
 
-    const getTasksFormMonth = (month: number) => {
+    const getTasksForMonth = (month: number) => {
         return BaseService.get(baseUrl + 'allTasks/' + month);
-    }
+    };
 
-    return {getTasksFormMonth}
-}
+    const getTaskForDay = (dayInMonth: number, month: number, year: number) => {
+        return BaseService.get(baseUrl + 'dailyTasks/' + dayInMonth + '/' + month + '/' + year);
+    };
+
+    const deleteTask = (id: string) => {
+        return BaseService.delete(baseUrl + '/' + id);
+    };
+
+    const getById = (id: string) => {
+        return BaseService.get(baseUrl + '/' + id);
+    };
+
+    const addTask = (taskData: MonthDataModel) => {
+        return BaseService.post<MonthDataModel>(baseUrl, taskData);
+    };
+
+    const updateTask = (taskData: MonthDataModel, id: string) => {
+        return BaseService.put(baseUrl + '/' + id, taskData);
+    };
+
+    return { getTasksForMonth, getTaskForDay, deleteTask, getById, addTask, updateTask };
+};
 
 export default TasksService();
